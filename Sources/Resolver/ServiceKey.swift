b3c93eff6c3210d141db34b1a5223dc703fefa71
Sukhrob Khakimov <sukhrob.khakimov@gmail.com>
2021-06-22 21:21:53 +0500

    Create `ServiceKey` to uniquely identify services

---
+++ Sources/Resolver/ServiceKey.swift
@@ -0,0 +1,21 @@
+struct ServiceKey: Hashable {
+    let serviceType: Any.Type
+    let name: String?
+    let argumentsType: Any.Type?
+
+    init(serviceType: Any.Type, name: String? = nil, argumentsType: Any.Type? = nil) {
+        self.serviceType = serviceType
+        self.name = name
+        self.argumentsType = argumentsType
+    }
+
+    func hash(into hasher: inout Hasher) {
+        ObjectIdentifier(serviceType).hash(into: &hasher)
+        name?.hash(into: &hasher)
+        if let argumentsType = argumentsType { ObjectIdentifier(argumentsType).hash(into: &hasher) }
+    }
+
+    static func ==(lhs: ServiceKey, rhs: ServiceKey) -> Bool {
+        lhs.serviceType == rhs.serviceType && lhs.name == rhs.name && lhs.argumentsType == rhs.argumentsType
+    }
+}

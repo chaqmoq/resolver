b6b64f13fdafd749adf67519fa8e8faca052febe
Sukhrob Khakimov <sukhrob.khakimov@gmail.com>
2021-06-22 21:23:22 +0500

    Enable to register services

--- Sources/Resolver/Resolver.swift
+++ Sources/Resolver/Resolver.swift
@@ -1,1 +1,26 @@
-public final class Resolver {}
+import Foundation
+
+public final class Resolver {
+    private var serviceFactories: [ServiceKey: Any] = .init()
+
+    public init() {}
+}
+
+extension Resolver {
+    public func register<Service>(
+        _ serviceType: Service.Type = Service.self,
+        name: String? = nil,
+        serviceFactory: @escaping (Resolver) -> Service
+    ) {
+        _register(serviceType, name: name, serviceFactory: serviceFactory)
+    }
+
+    func _register<Service, Arguments>(
+        _ serviceType: Service.Type = Service.self,
+        name: String? = nil,
+        serviceFactory: @escaping (Arguments) -> Service
+    ) {
+        let key = ServiceKey(serviceType: serviceType, name: name, argumentsType: Arguments.self)
+        serviceFactories[key] = serviceFactory
+    }
+}

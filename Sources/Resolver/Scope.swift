67a25467d923cf0dfa7e6109176cb2fbdcfa72b7
Sukhrob Khakimov <sukhrob.khakimov@gmail.com>
2021-07-09 10:21:48 +0500

    Conform `Scope` to `CustomStringConvertible` protocol

--- Sources/Resolver/Scope.swift
+++ Sources/Resolver/Scope.swift
@@ -1,7 +1,9 @@
-public enum Scope: String, CaseIterable {
+public enum Scope: String, CaseIterable, CustomStringConvertible {
     case cached
     case graph
     case shared
     case singleton
     case unique
+
+    public var description: String { rawValue }
 }

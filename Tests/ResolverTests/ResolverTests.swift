5aa66857c9928f8aace56fe7139ecf53e270761d
Sukhrob Khakimov <sukhrob.khakimov@gmail.com>
2021-07-10 14:49:36 +0500

    Test with one argument

--- Tests/ResolverTests/ResolverTests.swift
+++ Tests/ResolverTests/ResolverTests.swift
@@ -4,6 +4,8 @@ import XCTest
 final class ResolverTests: XCTestCase {
     let resolver = Resolver()
 
+    let arg1 = "string"
+
     func testRegisterAndResolve() {
         // Arrange
         let type = Service.self
@@ -19,6 +21,25 @@ final class ResolverTests: XCTestCase {
 
             // Assert
             XCTAssertNotNil(service)
+        }
+    }
+
+    func testRegisterAndResolveWithOneArgument() {
+        // Arrange
+        let type = ServiceWithOneArgument.self
+        var name = String(describing: type)
+
+        for scope in Scope.allCases {
+            // Arrange
+            name += scope.rawValue
+
+            // Act
+            resolver.register(type, named: name, scoped: scope) { _, arg1 in ServiceWithOneArgument(arg1: arg1) }
+            let service = resolver.resolve(type, named: name, arguments: arg1)
+
+            // Assert
+            XCTAssertNotNil(service)
+            XCTAssertEqual(service?.arg1, arg1)
         }
     }
 }
--- Tests/ResolverTests/Services.swift
+++ Tests/ResolverTests/Services.swift
@@ -1,1 +1,9 @@
 final class Service {}
+
+final class ServiceWithOneArgument {
+    let arg1: String
+
+    init(arg1: String) {
+        self.arg1 = arg1
+    }
+}

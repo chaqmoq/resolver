41cce505953454df8e50b85a43d3bad818fe56aa
Sukhrob Khakimov <sukhrob.khakimov@gmail.com>
2021-07-10 14:48:41 +0500

    Test without arguments

--- Tests/ResolverTests/ResolverTests.swift
+++ Tests/ResolverTests/ResolverTests.swift
@@ -1,4 +1,24 @@
 @testable import Resolver
 import XCTest
 
-final class ResolverTests: XCTestCase {}
+final class ResolverTests: XCTestCase {
+    let resolver = Resolver()
+
+    func testRegisterAndResolve() {
+        // Arrange
+        let type = Service.self
+        var name = String(describing: type)
+
+        for scope in Scope.allCases {
+            // Arrange
+            name += scope.rawValue
+
+            // Act
+            resolver.register(type, named: name, scoped: scope) { _ in Service() }
+            let service = resolver.resolve(type, named: name)
+
+            // Assert
+            XCTAssertNotNil(service)
+        }
+    }
+}

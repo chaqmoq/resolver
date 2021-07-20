f52a256ace5c277ecca042fbed76b7a02d9493dc
Sukhrob Khakimov <sukhrob.khakimov@gmail.com>
2021-07-20 16:16:23 +0500

    Fix resolving with the `main` resolver and multiple arguments

--- Sources/Resolver/Resolver+Arguments.swift
+++ Sources/Resolver/Resolver+Arguments.swift
@@ -218,7 +218,7 @@ extension Resolver {
         named name: String? = nil,
         arguments arg1: Arg1
     ) -> Service? {
-        main.resolve(type, named: name, arguments: (self, arg1)) { _ in }
+        main.resolve(type, named: name, arguments: arg1)
     }
 
     public static func resolve<Service, Arg1, Arg2>(
@@ -226,7 +226,7 @@ extension Resolver {
         named name: String? = nil,
         arguments arg1: Arg1, _ arg2: Arg2
     ) -> Service? {
-        main.resolve(type, named: name, arguments: (self, arg1, arg2)) { _ in }
+        main.resolve(type, named: name, arguments: arg1, arg2)
     }
 
     public static func resolve<Service, Arg1, Arg2, Arg3>(
@@ -234,7 +234,7 @@ extension Resolver {
         named name: String? = nil,
         arguments arg1: Arg1, _ arg2: Arg2, _ arg3: Arg3
     ) -> Service? {
-        main.resolve(type, named: name, arguments: (self, arg1, arg2, arg3)) { _ in }
+        main.resolve(type, named: name, arguments: arg1, arg2, arg3)
     }
 
     public static func resolve<Service, Arg1, Arg2, Arg3, Arg4>(
@@ -242,7 +242,7 @@ extension Resolver {
         named name: String? = nil,
         arguments arg1: Arg1, _ arg2: Arg2, _ arg3: Arg3, _ arg4: Arg4
     ) -> Service? {
-        main.resolve(type, named: name, arguments: (self, arg1, arg2, arg3, arg4)) { _ in }
+        main.resolve(type, named: name, arguments: arg1, arg2, arg3, arg4)
     }
 
     public static func resolve<Service, Arg1, Arg2, Arg3, Arg4, Arg5>(
@@ -250,7 +250,7 @@ extension Resolver {
         named name: String? = nil,
         arguments arg1: Arg1, _ arg2: Arg2, _ arg3: Arg3, _ arg4: Arg4, _ arg5: Arg5
     ) -> Service? {
-        main.resolve(type, named: name, arguments: (self, arg1, arg2, arg3, arg4, arg5)) { _ in }
+        main.resolve(type, named: name, arguments: arg1, arg2, arg3, arg4, arg5)
     }
 
     public static func resolve<Service, Arg1, Arg2, Arg3, Arg4, Arg5, Arg6>(
@@ -258,7 +258,7 @@ extension Resolver {
         named name: String? = nil,
         arguments arg1: Arg1, _ arg2: Arg2, _ arg3: Arg3, _ arg4: Arg4, _ arg5: Arg5, _ arg6: Arg6
     ) -> Service? {
-        main.resolve(type, named: name, arguments: (self, arg1, arg2, arg3, arg4, arg5, arg6)) { _ in }
+        main.resolve(type, named: name, arguments: arg1, arg2, arg3, arg4, arg5, arg6)
     }
 
     public static func resolve<Service, Arg1, Arg2, Arg3, Arg4, Arg5, Arg6, Arg7>(
@@ -266,7 +266,7 @@ extension Resolver {
         named name: String? = nil,
         arguments arg1: Arg1, _ arg2: Arg2, _ arg3: Arg3, _ arg4: Arg4, _ arg5: Arg5, _ arg6: Arg6, _ arg7: Arg7
     ) -> Service? {
-        main.resolve(type, named: name, arguments: (self, arg1, arg2, arg3, arg4, arg5, arg6, arg7)) { _ in }
+        main.resolve(type, named: name, arguments: arg1, arg2, arg3, arg4, arg5, arg6, arg7)
     }
 
     public static func resolve<Service, Arg1, Arg2, Arg3, Arg4, Arg5, Arg6, Arg7, Arg8>(
@@ -275,7 +275,7 @@ extension Resolver {
         arguments arg1: Arg1, _ arg2: Arg2, _ arg3: Arg3, _ arg4: Arg4, _ arg5: Arg5, _ arg6: Arg6, _ arg7: Arg7,
         _ arg8: Arg8
     ) -> Service? {
-        main.resolve(type, named: name, arguments: (self, arg1, arg2, arg3, arg4, arg5, arg6, arg7, arg8)) { _ in }
+        main.resolve(type, named: name, arguments: arg1, arg2, arg3, arg4, arg5, arg6, arg7, arg8)
     }
 
     public static func resolve<Service, Arg1, Arg2, Arg3, Arg4, Arg5, Arg6, Arg7, Arg8, Arg9>(
@@ -284,11 +284,7 @@ extension Resolver {
         arguments arg1: Arg1, _ arg2: Arg2, _ arg3: Arg3, _ arg4: Arg4, _ arg5: Arg5, _ arg6: Arg6, _ arg7: Arg7,
         _ arg8: Arg8, _ arg9: Arg9
     ) -> Service? {
-        main.resolve(
-            type,
-            named: name,
-            arguments: (self, arg1, arg2, arg3, arg4, arg5, arg6, arg7, arg8, arg9)
-        ) { _ in }
+        main.resolve(type, named: name, arguments: arg1, arg2, arg3, arg4, arg5, arg6, arg7, arg8, arg9)
     }
 
     public static func resolve<Service, Arg1, Arg2, Arg3, Arg4, Arg5, Arg6, Arg7, Arg8, Arg9, Arg10>(
@@ -297,11 +293,7 @@ extension Resolver {
         arguments arg1: Arg1, _ arg2: Arg2, _ arg3: Arg3, _ arg4: Arg4, _ arg5: Arg5, _ arg6: Arg6, _ arg7: Arg7,
         _ arg8: Arg8, _ arg9: Arg9, _ arg10: Arg10
     ) -> Service? {
-        main.resolve(
-            type,
-            named: name,
-            arguments: (self, arg1, arg2, arg3, arg4, arg5, arg6, arg7, arg8, arg9, arg10)
-        ) { _ in }
+        main.resolve(type, named: name, arguments: arg1, arg2, arg3, arg4, arg5, arg6, arg7, arg8, arg9, arg10)
     }
 }
 
@@ -311,7 +303,7 @@ extension Resolver {
         named name: String? = nil,
         arguments arg1: Arg1
     ) -> Service? {
-        resolve(type, named: name, arguments: (self, arg1)) { _ in }
+        doResolve(type, named: name, arguments: (self, arg1)) { _ in }
     }
 
     public func resolve<Service, Arg1, Arg2>(
@@ -319,7 +311,7 @@ extension Resolver {
         named name: String? = nil,
         arguments arg1: Arg1, _ arg2: Arg2
     ) -> Service? {
-        resolve(type, named: name, arguments: (self, arg1, arg2)) { _ in }
+        doResolve(type, named: name, arguments: (self, arg1, arg2)) { _ in }
     }
 
     public func resolve<Service, Arg1, Arg2, Arg3>(
@@ -327,7 +319,7 @@ extension Resolver {
         named name: String? = nil,
         arguments arg1: Arg1, _ arg2: Arg2, _ arg3: Arg3
     ) -> Service? {
-        resolve(type, named: name, arguments: (self, arg1, arg2, arg3)) { _ in }
+        doResolve(type, named: name, arguments: (self, arg1, arg2, arg3)) { _ in }
     }
 
     public func resolve<Service, Arg1, Arg2, Arg3, Arg4>(
@@ -335,7 +327,7 @@ extension Resolver {
         named name: String? = nil,
         arguments arg1: Arg1, _ arg2: Arg2, _ arg3: Arg3, _ arg4: Arg4
     ) -> Service? {
-        resolve(type, named: name, arguments: (self, arg1, arg2, arg3, arg4)) { _ in }
+        doResolve(type, named: name, arguments: (self, arg1, arg2, arg3, arg4)) { _ in }
     }
 
     public func resolve<Service, Arg1, Arg2, Arg3, Arg4, Arg5>(
@@ -343,7 +335,7 @@ extension Resolver {
         named name: String? = nil,
         arguments arg1: Arg1, _ arg2: Arg2, _ arg3: Arg3, _ arg4: Arg4, _ arg5: Arg5
     ) -> Service? {
-        resolve(type, named: name, arguments: (self, arg1, arg2, arg3, arg4, arg5)) { _ in }
+        doResolve(type, named: name, arguments: (self, arg1, arg2, arg3, arg4, arg5)) { _ in }
     }
 
     public func resolve<Service, Arg1, Arg2, Arg3, Arg4, Arg5, Arg6>(
@@ -351,7 +343,7 @@ extension Resolver {
         named name: String? = nil,
         arguments arg1: Arg1, _ arg2: Arg2, _ arg3: Arg3, _ arg4: Arg4, _ arg5: Arg5, _ arg6: Arg6
     ) -> Service? {
-        resolve(type, named: name, arguments: (self, arg1, arg2, arg3, arg4, arg5, arg6)) { _ in }
+        doResolve(type, named: name, arguments: (self, arg1, arg2, arg3, arg4, arg5, arg6)) { _ in }
     }
 
     public func resolve<Service, Arg1, Arg2, Arg3, Arg4, Arg5, Arg6, Arg7>(
@@ -359,7 +351,7 @@ extension Resolver {
         named name: String? = nil,
         arguments arg1: Arg1, _ arg2: Arg2, _ arg3: Arg3, _ arg4: Arg4, _ arg5: Arg5, _ arg6: Arg6, _ arg7: Arg7
     ) -> Service? {
-        resolve(type, named: name, arguments: (self, arg1, arg2, arg3, arg4, arg5, arg6, arg7)) { _ in }
+        doResolve(type, named: name, arguments: (self, arg1, arg2, arg3, arg4, arg5, arg6, arg7)) { _ in }
     }
 
     public func resolve<Service, Arg1, Arg2, Arg3, Arg4, Arg5, Arg6, Arg7, Arg8>(
@@ -368,7 +360,7 @@ extension Resolver {
         arguments arg1: Arg1, _ arg2: Arg2, _ arg3: Arg3, _ arg4: Arg4, _ arg5: Arg5, _ arg6: Arg6, _ arg7: Arg7,
         _ arg8: Arg8
     ) -> Service? {
-        resolve(type, named: name, arguments: (self, arg1, arg2, arg3, arg4, arg5, arg6, arg7, arg8)) { _ in }
+        doResolve(type, named: name, arguments: (self, arg1, arg2, arg3, arg4, arg5, arg6, arg7, arg8)) { _ in }
     }
 
     public func resolve<Service, Arg1, Arg2, Arg3, Arg4, Arg5, Arg6, Arg7, Arg8, Arg9>(
@@ -377,7 +369,7 @@ extension Resolver {
         arguments arg1: Arg1, _ arg2: Arg2, _ arg3: Arg3, _ arg4: Arg4, _ arg5: Arg5, _ arg6: Arg6, _ arg7: Arg7,
         _ arg8: Arg8, _ arg9: Arg9
     ) -> Service? {
-        resolve(type, named: name, arguments: (self, arg1, arg2, arg3, arg4, arg5, arg6, arg7, arg8, arg9)) { _ in }
+        doResolve(type, named: name, arguments: (self, arg1, arg2, arg3, arg4, arg5, arg6, arg7, arg8, arg9)) { _ in }
     }
 
     public func resolve<Service, Arg1, Arg2, Arg3, Arg4, Arg5, Arg6, Arg7, Arg8, Arg9, Arg10>(
@@ -386,7 +378,7 @@ extension Resolver {
         arguments arg1: Arg1, _ arg2: Arg2, _ arg3: Arg3, _ arg4: Arg4, _ arg5: Arg5, _ arg6: Arg6, _ arg7: Arg7,
         _ arg8: Arg8, _ arg9: Arg9, _ arg10: Arg10
     ) -> Service? {
-        resolve(
+        doResolve(
             type,
             named: name,
             arguments: (self, arg1, arg2, arg3, arg4, arg5, arg6, arg7, arg8, arg9, arg10)

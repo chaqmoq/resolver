2e3d28ecfb8aa98d5ea71bcc553743e8efc3cf79
Sukhrob Khakimov <sukhrob.khakimov@gmail.com>
2021-07-20 15:15:10 +0500

    Register a service with the `main` resolver and nine arguments

--- Sources/Resolver/Resolver+Arguments.swift
+++ Sources/Resolver/Resolver+Arguments.swift
@@ -78,6 +78,16 @@ extension Resolver {
     ) -> Resolver {
         main.register(type, named: name, scoped: scope, factory: factory)
     }
+
+    @discardableResult
+    public static func register<Service, Arg1, Arg2, Arg3, Arg4, Arg5, Arg6, Arg7, Arg8, Arg9>(
+        _ type: Service.Type = Service.self,
+        named name: String? = nil,
+        scoped scope: Scope = .graph,
+        factory: @escaping (Resolver, Arg1, Arg2, Arg3, Arg4, Arg5, Arg6, Arg7, Arg8, Arg9) -> Service
+    ) -> Resolver {
+        main.register(type, named: name, scoped: scope, factory: factory)
+    }
 }
 
 extension Resolver {

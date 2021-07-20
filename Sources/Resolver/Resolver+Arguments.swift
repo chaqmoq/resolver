70d0e0d5c8178ae51910eb65bd1d932f5a1bb612
Sukhrob Khakimov <sukhrob.khakimov@gmail.com>
2021-07-20 15:20:09 +0500

    Resolver a service with the `main` resolver and seven arguments

--- Sources/Resolver/Resolver+Arguments.swift
+++ Sources/Resolver/Resolver+Arguments.swift
@@ -260,6 +260,14 @@ extension Resolver {
     ) -> Service? {
         main.resolve(type, named: name, arguments: (self, arg1, arg2, arg3, arg4, arg5, arg6)) { _ in }
     }
+
+    public static func resolve<Service, Arg1, Arg2, Arg3, Arg4, Arg5, Arg6, Arg7>(
+        _ type: Service.Type = Service.self,
+        named name: String? = nil,
+        arguments arg1: Arg1, _ arg2: Arg2, _ arg3: Arg3, _ arg4: Arg4, _ arg5: Arg5, _ arg6: Arg6, _ arg7: Arg7
+    ) -> Service? {
+        main.resolve(type, named: name, arguments: (self, arg1, arg2, arg3, arg4, arg5, arg6, arg7)) { _ in }
+    }
 }
 
 extension Resolver {

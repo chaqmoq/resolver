2d313e37e5b75d4fb14cc64d5c8465fe3c483e72
Sukhrob Khakimov <sukhrob.khakimov@gmail.com>
2021-07-23 21:20:56 +0500

    Resolve an optional service with three arguments using `OptionalInjected` property wrapper

--- Sources/Resolver/PropertyWrapper/OptionalInjected.swift
+++ Sources/Resolver/PropertyWrapper/OptionalInjected.swift
@@ -17,4 +17,12 @@
     public init<Arg1, Arg2>(resolver: Resolver = .main, name: String? = nil, arguments arg1: Arg1, _ arg2: Arg2) {
         service = resolver.doResolve(Service.self, named: name, arguments: (resolver, arg1, arg2)) { _ in }
     }
+
+    public init<Arg1, Arg2, Arg3>(
+        resolver: Resolver = .main,
+        name: String? = nil,
+        arguments arg1: Arg1, _ arg2: Arg2, _ arg3: Arg3
+    ) {
+        service = resolver.doResolve(Service.self, named: name, arguments: (resolver, arg1, arg2, arg3)) { _ in }
+    }
 }

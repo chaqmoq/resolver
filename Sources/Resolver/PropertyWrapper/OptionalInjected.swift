7c57d2d4811080812144f581a52df6f0fc81e6a0
Sukhrob Khakimov <sukhrob.khakimov@gmail.com>
2021-07-23 21:18:05 +0500

    Create `OptionalInjected` property wrapper that resolves an optional service instantly

---
+++ Sources/Resolver/PropertyWrapper/OptionalInjected.swift
@@ -0,0 +1,12 @@
+@propertyWrapper public struct OptionalInjected<Service> {
+    public var wrappedValue: Service? {
+        get { service }
+        set { service = newValue }
+    }
+
+    private var service: Service?
+
+    public init(resolver: Resolver = .main, name: String? = nil) {
+        service = resolver.resolve(Service.self, named: name)
+    }
+}

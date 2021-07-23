22312a7e3bc3f34fcccc06554e7737682166d70f
Sukhrob Khakimov <sukhrob.khakimov@gmail.com>
2021-07-23 21:22:52 +0500

    Resolve an optional service with nine arguments using `OptionalInjected` property wrapper

--- Sources/Resolver/PropertyWrapper/OptionalInjected.swift
+++ Sources/Resolver/PropertyWrapper/OptionalInjected.swift
@@ -86,4 +86,17 @@
             arguments: (resolver, arg1, arg2, arg3, arg4, arg5, arg6, arg7, arg8)
         ) { _ in }
     }
+
+    public init<Arg1, Arg2, Arg3, Arg4, Arg5, Arg6, Arg7, Arg8, Arg9>(
+        resolver: Resolver = .main,
+        name: String? = nil,
+        arguments arg1: Arg1, _ arg2: Arg2, _ arg3: Arg3, _ arg4: Arg4, _ arg5: Arg5, _ arg6: Arg6, _ arg7: Arg7,
+        _ arg8: Arg8, _ arg9: Arg9
+    ) {
+        service = resolver.doResolve(
+            Service.self,
+            named: name,
+            arguments: (resolver, arg1, arg2, arg3, arg4, arg5, arg6, arg7, arg8, arg9)
+        ) { _ in }
+    }
 }

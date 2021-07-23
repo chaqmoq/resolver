9e4ccedb3c8ab02e0409f8f2096fa7a78de2c451
Sukhrob Khakimov <sukhrob.khakimov@gmail.com>
2021-07-23 21:06:53 +0500

    Resolve a service with six arguments using `Injected` property wrapper

--- Sources/Resolver/PropertyWrapper/Injected.swift
+++ Sources/Resolver/PropertyWrapper/Injected.swift
@@ -49,4 +49,16 @@
             arguments: (resolver, arg1, arg2, arg3, arg4, arg5)
         ) { _ in }!
     }
+
+    public init<Arg1, Arg2, Arg3, Arg4, Arg5, Arg6>(
+        resolver: Resolver = .main,
+        name: String? = nil,
+        arguments arg1: Arg1, _ arg2: Arg2, _ arg3: Arg3, _ arg4: Arg4, _ arg5: Arg5, _ arg6: Arg6
+    ) {
+        service = resolver.doResolve(
+            Service.self,
+            named: name,
+            arguments: (resolver, arg1, arg2, arg3, arg4, arg5, arg6)
+        ) { _ in }!
+    }
 }

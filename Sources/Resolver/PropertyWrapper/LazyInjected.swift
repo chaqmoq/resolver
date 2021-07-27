3c6980e1c8d654f55d1609313c6132cfdea17e18
Sukhrob Khakimov <sukhrob.khakimov@gmail.com>
2021-07-27 10:17:28 +0500

    Resolve a service using `LazyInjected` property wrapper

---
+++ Sources/Resolver/PropertyWrapper/LazyInjected.swift
@@ -0,0 +1,37 @@
+import Foundation
+
+@propertyWrapper public struct LazyInjected<Service> {
+    public var wrappedValue: Service {
+        mutating get {
+            if resolver.isAtomic {
+                resolver.lock.sync {
+                    if service == nil {
+                        service = resolver.doResolve(Service.self, named: name, arguments: resolver)
+                    }
+                }
+            } else {
+                if service == nil {
+                    service = resolver.doResolve(Service.self, named: name, arguments: resolver)
+                }
+            }
+
+            return service
+        }
+        set {
+            if resolver.isAtomic {
+                resolver.lock.sync { service = newValue }
+            } else {
+                service = newValue
+            }
+        }
+    }
+
+    private let resolver: Resolver
+    private let name: String?
+    private var service: Service!
+
+    public init(resolver: Resolver = .main, name: String? = nil) {
+        self.resolver = resolver
+        self.name = name
+    }
+}

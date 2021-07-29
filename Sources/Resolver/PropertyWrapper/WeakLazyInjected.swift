d36b0c51357b36f578de01176b7e03e9fb49e8bc
Sukhrob Khakimov <sukhrob.khakimov@gmail.com>
2021-07-29 21:19:43 +0500

    Resolve an optional reference type service using `WeakLazyInjected` property wrapper

---
+++ Sources/Resolver/PropertyWrapper/WeakLazyInjected.swift
@@ -0,0 +1,35 @@
+@propertyWrapper public struct WeakLazyInjected<Service: AnyObject> {
+    public var wrappedValue: Service? {
+        mutating get {
+            if resolver.isAtomic {
+                resolver.lock.sync {
+                    if service == nil {
+                        service = resolver.doResolve(Service.self, named: name, arguments: resolver)
+                    }
+                }
+            } else {
+                if service == nil {
+                    service = resolver.doResolve(Service.self, named: name, arguments: resolver)
+                }
+            }
+
+            return service
+        }
+        set {
+            if resolver.isAtomic {
+                resolver.lock.sync { service = newValue }
+            } else {
+                service = newValue
+            }
+        }
+    }
+
+    private let resolver: Resolver
+    private let name: String?
+    private weak var service: Service?
+
+    public init(resolver: Resolver = .main, name: String? = nil) {
+        self.resolver = resolver
+        self.name = name
+    }
+}

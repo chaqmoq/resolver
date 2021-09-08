fc54748ecc3632ed8e996ee8b6615474d71706d1
Sukhrob Khakimov <sukhrob.khakimov@gmail.com>
2021-09-08 19:16:08 +0500

    Add documentation to `Scope`

--- Sources/Resolver/Scope.swift
+++ Sources/Resolver/Scope.swift
@@ -1,5 +1,20 @@
+/// A configuration option that determines how a new instance of a service is created.
 public enum Scope: String, CaseIterable, CustomStringConvertible {
-    case cached, graph, shared, singleton, unique
+    /// Creates one instance that exists for lifetime of an app or until the cache is reset or it is evicted from the cache due to memory pressure.
+    case cached
 
+    /// Creates one instance during a given resolution cycle.
+    case graph
+
+    /// Creates one instance that exists while a strong reference to it exists. Only works with reference types.
+    case shared
+
+    /// Creates one instance that exists for lifetime of an app.
+    case singleton
+
+    /// Always creates a new instance.
+    case unique
+
+    /// See `CustomStringConvertible`.
     public var description: String { rawValue }
 }

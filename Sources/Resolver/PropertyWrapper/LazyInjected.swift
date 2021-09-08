55acd24d88efd09e6a50c3e3c2c30a6fa02f1370
Sukhrob Khakimov <sukhrob.khakimov@gmail.com>
2021-09-08 21:17:23 +0500

    Add documentation to `LazyInjected` property wrapper

--- Sources/Resolver/PropertyWrapper/LazyInjected.swift
+++ Sources/Resolver/PropertyWrapper/LazyInjected.swift
@@ -1,5 +1,7 @@
+/// Injects and resolves a service when accessed for the first time.
 @propertyWrapper
 public struct LazyInjected<Service> {
+    /// A wrapped value.
     public var wrappedValue: Service {
         mutating get {
             if resolver.isAtomic {
@@ -25,6 +27,11 @@ public struct LazyInjected<Service> {
     private let name: String?
     private var service: Service!
 
+    /// Initializes a new instance of a service lazily.
+    ///
+    /// - Parameters:
+    ///   - resolver: An instance of `Resolver`.  Defaults to `main`.
+    ///   - name: The name of a service. Defaults to `nil`.
     public init(resolver: Resolver = .main, name: String? = nil) {
         self.resolver = resolver
         self.name = name

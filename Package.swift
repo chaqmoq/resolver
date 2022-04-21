50940c8b6268e28a030940c0fb72a85538558094
Sukhrob Khakimov <sukhrob.khakimov@gmail.com>
2022-04-21 22:17:08 +0500

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -4,10 +4,14 @@ import PackageDescription
 
 let package = Package(
     name: "chaqmoq-resolver",
-    products: [.library(name: "Resolver", targets: ["Resolver"])],
+    products: [
+        .library(name: "Resolver", targets: ["Resolver"])
+    ],
     targets: [
         .target(name: "Resolver"),
-        .testTarget(name: "ResolverTests", dependencies: [.target(name: "Resolver")])
+        .testTarget(name: "ResolverTests", dependencies: [
+            .target(name: "Resolver")
+        ])
     ],
     swiftLanguageVersions: [.v5]
 )
